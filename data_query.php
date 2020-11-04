65d37d036b62ae1bd195a249236f11c59f65f2ee
root <fr3shnuts@gmail.com>
2020-11-03 21:31:16 -0800

    echo format

--- data_query.php
+++ data_query.php
@@ -1,4 +1,5 @@
 <?php
+// 'root'@'localhost' w/ no password = permission denied
 $servername = "localhost";
 $username = "www-data";
 $password = "password";
@@ -22,13 +23,13 @@ if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
 
-echo "<br>Gamer_Tag: " . $row["Gamer_Tag"];
-echo "<br>Season Total: " . $row["Season_Total"];
-echo "<br>Season Eliminations: " . $row["Season_Elim"];
-echo "<br>Season Placement:  " . $row["Season_Placement"];
-echo "<br>Season Appearances: " . $row["Season_Appear"]; 
-echo "<br>Seed Score: " . $row["Seed_Score"];
-echo "<br>Lifetime Earnings: " . $row["Lifetime_Earnings"];
+echo "<br>Gamer_Tag: "            . $row["Gamer_Tag"];
+echo "<br>Season Total: "         . $row["Season_Total"];
+echo "<br>Season Eliminations: "  . $row["Season_Elim"];
+echo "<br>Season Placement:  "    . $row["Season_Placement"];
+echo "<br>Season Appearances: "   . $row["Season_Appear"]; 
+echo "<br>Seed Score: "           . $row["Seed_Score"];
+echo "<br>Lifetime Earnings: "    . $row["Lifetime_Earnings"];
 echo "<br>";
   }
 } else {
