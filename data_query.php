c62e8b2c34e6797f220e72636a259aae9a45bc71
Freshnuts <fr3shnuts@gmail.com>
2020-11-11 04:02:40 -0800

    Update

--- data_query.php
+++ data_query.php
@@ -22,15 +22,14 @@ echo "<h2>Fortnite Player Statistics</h2><br>";
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-
-echo "<br>Gamer_Tag: "            . $row["Gamer_Tag"];
-echo "<br>Season Total: "         . $row["Season_Total"];
-echo "<br>Season Eliminations: "  . $row["Season_Elim"];
-echo "<br>Season Placement:  "    . $row["Season_Placement"];
-echo "<br>Season Appearances: "   . $row["Season_Appear"]; 
-echo "<br>Seed Score: "           . $row["Seed_Score"];
-echo "<br>Lifetime Earnings: "    . $row["Lifetime_Earnings"];
-echo "<br>";
+    echo "<br>Gamer_Tag: "            . $row["Gamer_Tag"];
+    echo "<br>Season Total: "         . $row["Season_Total"];
+    echo "<br>Season Eliminations: "  . $row["Season_Elim"];
+    echo "<br>Season Placement:  "    . $row["Season_Placement"];
+    echo "<br>Season Appearances: "   . $row["Season_Appear"]; 
+    echo "<br>Seed Score: "           . $row["Seed_Score"];
+    echo "<br>Lifetime Earnings: "    . $row["Lifetime_Earnings"];
+    echo "<br>";
   }
 } else {
 
